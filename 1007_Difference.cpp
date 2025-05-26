2da5a5a22d42af8e9adfd63c115f5b2a8579a493
NicolauHS <nicolash0204@gmail.com>
2025-05-26 15:58:03 -0300

    added solution to problem 1007

---
+++ 1007_Difference.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int a, b, c, d, res;
+	
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	
+	res = (a * b) - (c * d);
+	
+	printf("DIFERENCA = %d\n", res);
+	
+	return 0;
+}

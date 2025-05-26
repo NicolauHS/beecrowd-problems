62a7fb86a21d24c0c3d0b30549c6651da6c78168
NicolauHS <nicolash0204@gmail.com>
2025-05-26 15:58:01 -0300

    added solution to problem 1002

---
+++ 1002_Area_of_a_Circle.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+	double R;
+	double n = 3.14159;
+	
+	cin >> R;
+	
+	double res = n * pow(R, 2);
+	
+	printf("A=%.4f\n", res);
+	
+	return 0;
+}
